989beff5a07c5588e0976f13daac16fbb6830aa1
Vladislavovna2407 <Vladislavovna2018@bk.ru>
2022-11-06 18:21:21 +0300

    Update script.js
    Написала код для списка продуктов.

--- script.js
+++ script.js
@@ -7,3 +7,32 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const input = document.querySelector('#input');
+const container = document.querySelector('#items')
+
+input.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+
+        const product = input.value;
+
+        const newProduct = document.createElement('p');
+        newProduct.textContent = product;
+
+        if (product != '') {
+            container.prepend(newProduct);
+        }
+
+        input.value = '';
+
+
+    }
+    const elements = document.querySelectorAll('p')
+    console.log(elements)
+    for (let item of elements) {
+        item.addEventListener('click', function() {
+            item.classList.toggle('done');
+        });
+    }
+
+});
